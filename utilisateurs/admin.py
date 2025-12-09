f27881de8a4a5b4427d4e1510fe082c042166922
abi-seg <abirami1488@gmail.com>
2025-12-09 23:49:48 +0100

    Register Utilisateur model in admin.py

--- utilisateurs/admin.py
+++ utilisateurs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Utilisateur
+
+@admin.register(Utilisateur)
+class UtilisateurAdmin(UserAdmin):
+    model = Utilisateur
+    list_display = ('username', 'age', 'peut_etre_contacte', 'partage_donnees', 'is_staff')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informations supplémentaires', {
+            'fields': ('age', 'peut_etre_contacte', 'partage_donnees')
+        }),
+    )
 
 # Register your models here.
