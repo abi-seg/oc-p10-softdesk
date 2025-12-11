744051281ee43393483e2583a0096212f4565ff5
abi-seg <abirami1488@gmail.com>
2025-12-11 10:13:15 +0100

    Added validation to prevent duplicate project titles per user

--- projets/serializers.py
+++ projets/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True) #display author's username
     class Meta:
         model = Project
-        fields = ['id','title', 'description', 'type_projet', 'author', 'created_at']
+        fields = ['id','title', 'description', 'type', 'author', 'created_at']
         read_only_fields = ['id', 'author', 'created_at']
+
+    def validate_title(self, value):
+        user = self.context['request'].user
+        if Project.objects.filter(title=value, author=user).exists():
+            raise serializers.ValidationError("You already have a project with this title.")    
+        return value
