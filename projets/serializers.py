e804475abbb64e314ffcbb69564ed1bdfebb9e39
abi-seg <abirami1488@gmail.com>
2025-12-11 22:36:31 +0100

    Add IssueViewset and Serializers

--- projets/serializers.py
+++ projets/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Project, Contributor, Utilisateur
+from .models import Project, Contributor, Utilisateur,Issue
 
 class ProjectSerializer(serializers.ModelSerializer):
 
@@ -32,4 +32,25 @@ class ContributorSerializer(serializers.ModelSerializer):
         rep = super().to_representation(instance)
         rep['user'] = instance.user.username  # Display username instead of ID
         rep['project'] = instance.project.title  # Display project title instead of ID
+        return rep
+    
+class IssueSerializer(serializers.ModelSerializer):
+    assigned_to = serializers.PrimaryKeyRelatedField(
+        queryset=Utilisateur.objects.all(),
+        
+    )
+    project = serializers.PrimaryKeyRelatedField(
+        queryset=Project.objects.all() 
+    )
+
+    class Meta:
+        model = Issue
+        fields = ['id', 'title', 'description', 'tag', 'priority', 'status', 'author', 'assigned_to', 'project', 'created_at']
+        read_only_fields = ['id', 'author',  'created_at']
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['author'] = instance.author.username  # Display author's username
+        rep['assigned_to'] = instance.assigned_to.username if instance.assigned_to else None  # Display assignee's username
+        rep['project'] = instance.project.title  # Display project title
         return rep
--- projets/views.py
+++ projets/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions, serializers
-from .models import Project, Contributor, Utilisateur
-from .serializers import ProjectSerializer, ContributorSerializer
+from .models import Project, Contributor, Utilisateur, Issue
+from .serializers import ProjectSerializer, ContributorSerializer, IssueSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
@@ -49,3 +49,29 @@ class ContributorViewSet(viewsets.ModelViewSet):
             serializer.save(project=project, user=user)
         except (Project.DoesNotExist, Utilisateur.DoesNotExist):
             raise serializers.ValidationError("Invalid user or project ID.")
+        
+class IssueViewSet(viewsets.ModelViewSet):
+    serializer_class = IssueSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff or user.is_superuser:
+            return Issue.objects.all()
+        
+        return Issue.objects.filter(project__author=self.request.user) | Issue.objects.filter(assigned_to=self.request.user)
+    
+    def perform_create(self, serializer):
+        project_id = self.request.data.get('project')
+        assigned_to_id = self.request.data.get('assigned_to')
+
+        try:
+            project = Project.objects.get(id=project_id)
+            assigned_to = Utilisateur.objects.get(id=assigned_to_id) if assigned_to_id else None
+
+            if project.author != self.request.user:
+                raise serializers.ValidationError("You are not the author of this project.")
+
+            serializer.save(project=project, author=self.request.user, assigned_to=assigned_to)
+        except (Project.DoesNotExist, Utilisateur.DoesNotExist):
+            raise serializers.ValidationError("Invalid project or assignee ID.")
